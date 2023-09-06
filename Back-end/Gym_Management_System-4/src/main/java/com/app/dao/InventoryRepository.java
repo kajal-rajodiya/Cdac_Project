10f5130809a1fd47103e49779f6a3ce901451b9e
Kajal-rajodiya <kajalrajodiya1@gmail.com>
2023-09-06 14:22:25 +0530

    commit

--- Back-end/Gym_Management_System-4/src/main/java/com/app/dao/InventoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Inventory;
-
-public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
-
-}
--- Back-end/Gym_Management_System-4/src/main/java/com/app/dao/PlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Plan;
-
-public interface PlanRepository extends JpaRepository<Plan, Integer> {
-
-}
